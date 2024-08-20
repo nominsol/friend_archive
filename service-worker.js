eb2aba1ae787f04b3fae30ea28e38d991deed652
nominsol <xmxmxm3577@naver.com>
2024-08-21 02:01:15 +0900

    change s-worker

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,63 @@
-// service-worker.js
+const CACHE_NAME = 'my-cache-v1';
+const URLs_TO_CACHE = [
+    '/',
+    '/index.html', ,
+    '/assets/css/main.css',
+    '/assets/js/main.js',
+    '/assets/img/appIcon.png',
+    '/assets/img/main_picture.jpg' // 추가된 이미지
+];
 
 self.addEventListener('install', function(event) {
     console.log('Service worker installing...');
-    // Pre-cache some assets
     event.waitUntil(
-        caches.open('my-cache').then(function(cache) {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/b_urDay.html',
-                '/f_archive.html',
-                '/assets/css/main.css',
-                '/assets/js/main.js',
-                '/assets/js/b_urDay.js',
-                '/assets/js/f_archive.js',
-                '/assets/img/appIcon.png',
-            ]);
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(URLs_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    console.log('Service Worker activating.');
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
     );
 });
 
 self.addEventListener('fetch', function(event) {
+    if (event.request.method !== 'GET') {
+        // Non-GET requests are not handled by this service worker
+        event.respondWith(fetch(event.request));
+        return;
+    }
+
     event.respondWith(
         caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
+            if (response) {
+                // If there's a cached response, return it
+                return response;
+            }
+
+            // Otherwise, fetch from the network
+            return fetch(event.request).then(function(networkResponse) {
+                // Update the cache with the network response
+                return caches.open(CACHE_NAME).then(function(cache) {
+                    cache.put(event.request, networkResponse.clone());
+                    return networkResponse;
+                });
+            }).catch(function() {
+                // Handle fetch errors here
+                return new Response('Network error occurred', { status: 504 });
+            });
         })
     );
 });
